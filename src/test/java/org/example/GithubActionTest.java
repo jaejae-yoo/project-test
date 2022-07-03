6d6850316664fb72dd1ee3e31601de341a4f3738
jaejae-yoo <wotj102@gmail.com>
2022-07-03 13:18:52 +0900

    실패 테스트

---
+++ src/test/java/org/example/GithubActionTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GithubActionTest {
+
+    @DisplayName("실패 테스트 작성")
+    @Test
+    void fail() {
+        String test = "테스트";
+        assertThat(test).isEqualTo("");
+    }
+
+    @DisplayName("성공 테스트 작성")
+    @Test
+    void pass() {
+        String test = "테스트";
+        assertThat(test).isEqualTo("테스트");
+    }
+}
