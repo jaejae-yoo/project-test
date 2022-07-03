bae263af49f102ca60acec694cd31e079e4b36bf
jaejae-yoo <wotj102@gmail.com>
2022-07-03 13:20:58 +0900

    실패 -> 성공 테스트

--- src/test/java/org/example/GithubActionTest.java
+++ src/test/java/org/example/GithubActionTest.java
@@ -7,11 +7,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class GithubActionTest {
 
-    @DisplayName("실패 테스트 작성")
+    @DisplayName("실패 테스트 -> 성공 작성")
     @Test
     void fail() {
         String test = "테스트";
-        assertThat(test).isEqualTo("");
+        assertThat(test).isEqualTo("테스트");
     }
 
     @DisplayName("성공 테스트 작성")
